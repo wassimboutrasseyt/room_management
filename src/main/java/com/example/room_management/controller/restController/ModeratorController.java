4a5d92b187099d6e1922f60179678a2ed1ea40fa
BOUTRASSEYT WASSIM <110347714+wassimboutrasseyt@users.noreply.github.com>
2025-06-09 22:31:24 +0100

    Update ModeratorController.java

--- src/main/java/com/example/room_management/controller/restController/ModeratorController.java
+++ src/main/java/com/example/room_management/controller/restController/ModeratorController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("moderatorRoomManage")
+@RequestMapping("moderatorRoomManager")
 public class ModeratorController {
 
     private final ModeratorService moderatorService;
